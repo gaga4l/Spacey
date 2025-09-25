a0f81c47292979ad18e67d9da204dcd52732d981
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-09-25 10:22:08 +0300

    finished the navigation on crew page

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ let nowDot = document.querySelectorAll(".commander");
 
 dots.forEach((dot) => {
   dot.addEventListener("click", () => {
-    document
-      .getElementById(`${nowDot[1].className}`)
-      .setAttribute("aria-selected", false);
+    document.querySelector(".dots[aria-selected='true']")
+      ?.setAttribute("aria-selected", false);
     nowDot.forEach((now) => {
       now.setAttribute("hidden", "");
     });
     nowDot = document.querySelectorAll(`.${dot.getAttribute("id")}`);
-    document
-      .getElementById(`${nowDot[1].className}`)
-      .setAttribute("aria-selected", true);
+    dot.setAttribute("aria-selected", true);
     nowDot.forEach((now) => {
       now.removeAttribute("hidden", "");
     });
@@ -25,16 +22,13 @@ dots.forEach((dot) => {
 
 tabList.forEach((tab) => {
   tab.addEventListener("click", () => {
-    document
-      .getElementById(`${nowTab[0].className}`)
-      .setAttribute("aria-selected", false);
+    document.querySelector(".tab-list[aria-selected='true']")
+      ?.setAttribute("aria-selected", false);
     nowTab.forEach((now) => {
       now.setAttribute("hidden", "");
     });
     nowTab = document.querySelectorAll(`.${tab.getAttribute("id")}`);
-    document
-      .getElementById(`${nowTab[0].className}`)
-      .setAttribute("aria-selected", true);
+    tab.setAttribute("aria-selected", true);
     nowTab.forEach((now) => {
       now.removeAttribute("hidden", "");
     });
