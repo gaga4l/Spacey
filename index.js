50306f9dcfaac3297b66f695ef36c09cb1bb4370
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-09-25 08:06:36 +0300

    Working on navigation among  pages on crew page

--- index.js
+++ index.js
@@ -2,6 +2,26 @@ const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-app-navigation");
 const tabList = document.querySelectorAll(".tab-list");
 let nowTab = document.querySelectorAll(".moon");
+const dots = document.querySelectorAll(".dots");
+let nowDot = document.querySelectorAll(".commander");
+
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => {
+    document
+      .getElementById(`${nowDot[1].className}`)
+      .setAttribute("aria-selected", false);
+    nowDot.forEach((now) => {
+      now.setAttribute("hidden", "");
+    });
+    nowDot = document.querySelectorAll(`.${dot.getAttribute("id")}`);
+    document
+      .getElementById(`${nowDot[1].className}`)
+      .setAttribute("aria-selected", true);
+    nowDot.forEach((now) => {
+      now.removeAttribute("hidden", "");
+    });
+  });
+});
 
 tabList.forEach((tab) => {
   tab.addEventListener("click", () => {
@@ -10,7 +30,6 @@ tabList.forEach((tab) => {
       .setAttribute("aria-selected", false);
     nowTab.forEach((now) => {
       now.setAttribute("hidden", "");
-      console.log(nowTab);
     });
     nowTab = document.querySelectorAll(`.${tab.getAttribute("id")}`);
     document
