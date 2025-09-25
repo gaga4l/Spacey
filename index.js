acffd66ec1f4809df09b28e25130e9bc5d866fde
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-09-25 07:05:07 +0300

    Finished navigation between tab-lists on destination page

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-app-navigation");
+const tabList = document.querySelectorAll(".tab-list");
+let nowTab = document.querySelectorAll(".moon");
+
+tabList.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    document
+      .getElementById(`${nowTab[0].className}`)
+      .setAttribute("aria-selected", false);
+    nowTab.forEach((now) => {
+      now.setAttribute("hidden", "");
+      console.log(nowTab);
+    });
+    nowTab = document.querySelectorAll(`.${tab.getAttribute("id")}`);
+    document
+      .getElementById(`${nowTab[0].className}`)
+      .setAttribute("aria-selected", true);
+    nowTab.forEach((now) => {
+      now.removeAttribute("hidden", "");
+    });
+  });
+});
 
 navToggle.addEventListener("click", () => {
   const curr = nav.getAttribute("data-visible");
